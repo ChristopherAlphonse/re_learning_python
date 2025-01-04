3e50da531a99a6cb8c47eec424917cb16d610b3e
Christopher Alphonse <christopheralphonse96@gmail.com>
2025-01-04 18:40:31 -0500

    Update interstate_highway_numbers.py

--- Lab/interstate_highway_numbers.py
+++ Lab/interstate_highway_numbers.py
@@ -1,57 +1,66 @@
+# Primary U.S. interstate highways are numbered 1-99. Odd numbers (like the 5 or 95) go north/south, and evens (like the 10 or 90) go east/west. Auxiliary highways are numbered 100-999, and service the primary highway indicated by the rightmost two digits. Thus, I-405 services I-5, and I-290 services I-90. Note: 200 is not a valid auxiliary highway because 00 is not a valid primary highway number.
+
+# Given a highway number, indicate whether it is a primary or auxiliary highway. If auxiliary, indicate what primary highway it serves. Also indicate if the (primary) highway runs north/south or east/west.
+
+# Ex: If the input is:
+
+# 90
+# the output is:
+
+# I-90 is primary, going east/west.
+# Ex: If the input is:
+
+# 290
+# the output is:
+
+# I-290 is auxiliary, serving I-90, going east/west.
+# Ex: If the input is:
+
+# 0
+# the output is:
+
+# 0 is not a valid interstate highway number. 
+# Ex: If the input is:
+
+# 200
+# the output is:
+
+# 200 is not a valid interstate highway number.
+
+
 
 highway_number = int(input())
 
-# Facts: primary highways are numbered 1-99
-# IS_AUXiliary highways are numbered 100-999
-
+''' Type your code here. '''
 # Odd numbers (like the 5 or 95) go north/south
-# evens (like the 10 or 90) go east/west
-
-# Note: 200 is not a valid IS_AUXiliary highway because 00 is not a valid primary highway number
-
-# Q: Given a highway number, indicate whether it is a primary or IS_AUXiliary highway.
-# If IS_AUXiliary, indicate what primary highway it serves.
-# Also indicate if the (primary) highway runs north/south or east/west.
-
-# Ex: If the input is :
-
-# 90
-# I-90 is primary, going east/west.
+# evens (like the 10 or 90) go east/west. 
+# Thus, I-405 services I-5, and I-290 services I-90. 
 
 
-# 290
-# I-290 is IS_AUXiliary, serving I-90, going east/west.
+stringify_HN = str(highway_number)
+
+def get_direction(n):
+    return "east/west."if n % 2==0 else "north/south."
+    
+
+route = get_direction(highway_number)
 
 
-# 0 or 200
-# 0 or 200 is not a valid interstate highway number.
-
-
-def mod_two(n) -> bool:
-    return n % 2 == 0
-
-
-IS_AUX = "is IS_AUXiliary"
-IS_PRIMARY = "is primary"
-EAST_WEST = "east/west."
-NORTH_SOUTH = "north/south."
-
-
-def get_direction(n) -> str:
-    return EAST_WEST if mod_two(n) else NORTH_SOUTH
-
-
-MY_STR = str(highway_number)
-MY_ROUTE = get_direction(highway_number)
-
-if highway_number < 1 or '00' in MY_STR:
+if highway_number < 1 or '00' in stringify_HN:
     print(f"{highway_number} is not a valid interstate highway number.")
 
-elif highway_number in range(1, 100):
-    print(f"I-{highway_number} {IS_PRIMARY}, going {MY_ROUTE}")
+elif highway_number in range (1, 100):
+    
+    # interstate
+    if highway_number % 5==0:
+        
+        print(f"I-{highway_number} is primary, going {route}")
+    else:
+         print(f"I-{highway_number} is primary, going east/west.")
 else:
-    OUT_PUT = str(
-        f"I-{highway_number} {IS_AUX}, serving I-{highway_number % 100}, going {MY_ROUTE}")
-    if highway_number % 2 == 0 and highway_number >= 100:
-        print(OUT_PUT)
-    print(OUT_PUT)
+    #aux
+    direction = str(f"I-{highway_number} is auxiliary, serving I-{highway_number % 100}, going {route}")
+    if highway_number % 5 == 0 and highway_number >=100:
+        print(direction)
+    else:
+        print(direction)
