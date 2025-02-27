11eb996dee11550b1e44e8746470c6cfefa365ec
Christopher Alphonse <>
2025-02-26 20:52:29 -0500

    Add run-length encoding and decoding functions

--- DSA/run_length_encoding.py
+++ DSA/run_length_encoding.py
@@ -1,8 +1,3 @@
-# s = "aaaaaabbbbbccca"
-# s = "000011112225"
-s = ""
-
-
 def rle_encode(s):
     if not s:
         return s
@@ -20,4 +15,23 @@ def rle_encode(s):
     return ''.join(res)
 
 
-print(rle_encode(s))
+def rle_decode(encoded_string: str) -> str:
+    if not encoded_string:
+        return encoded_string
+
+    res = []
+    i = 0
+    while i < len(encoded_string):
+        length = int(encoded_string[i])
+        char = encoded_string[i + 1]
+        res.append(char * length)
+        i += 2
+    return ''.join(res)
+
+
+s = "aaaaaabbbbbccca"
+encoded_s = rle_encode(s)
+print(f"Encoded: {encoded_s}")
+
+decoded_s = rle_decode(encoded_s)
+print(f"Decoded: {decoded_s}")
